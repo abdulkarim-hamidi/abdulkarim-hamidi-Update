0693b3ef11b4f8f6b581763a58354f52b8ba2eb8
abdulkarim-hamidi <abdulkarim.hamidi@yahoo.com>
2023-02-22 23:03:14 +0000

    lab completed

--- src/main/java/UpdateARecord.java
+++ src/main/java/UpdateARecord.java
@@ -9,15 +9,20 @@ import java.sql.Statement;
 /**
  * SQL sublanguage: DML (Data Manipulation Language)
  *
- * Now that we know how to insert and remove records from a table, we are going to learn how to update a record.
+ * Now that we know how to insert and remove records from a table, we 
+ are going to learn how to update a record.
  *
- * To update a record we utilize the UPDATE keyword. The syntax for updating a record is as follows:
- * UPDATE table_name SET col_1 = val_1, col_2 = val_2, ...col_N = val_N WHERE condition;
+ * To update a record we utilize the UPDATE keyword. The syntax for 
+ updating a record is as follows:
+ * UPDATE table_name SET col_1 = val_1, col_2 = val_2, ...
+ * col_N = val_N WHERE condition;
  *
- * NOTE: The WHERE condition is important because if you leave this out, that column will be updated throughout all the records in the table.
+ * NOTE: The WHERE condition is important because if you leave 
+ * this out, that column will be updated throughout all the records in the table.
  *
  *
- * Additional Resource if needed: https://www.w3schools.com/sql/sql_update.asp
+ * Additional Resource if needed: 
+ * https://www.w3schools.com/sql/sql_update.asp
  *
  */
 public class UpdateARecord {
